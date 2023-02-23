fd862662e7e89de476b01e811149c0107900048e
Ruth Ramya <ramya.b228@gmail.com>
2023-02-23 15:25:20 +0530

    added moving to next question functionality

---
+++ script.js
@@ -0,0 +1,34 @@
+"use Strict";
+
+const nextBtn = document.querySelector(".next-btn");
+const prevBtn = document.querySelector(".prev-btn");
+const questionBoxes = document.querySelectorAll(".box");
+// console.log(questionBoxes);
+
+//adding functionlity to next and prev buttons
+
+let currentBox = 0;
+const moveBoxes = function (currentBox) {
+  questionBoxes.forEach((box, i) => {
+    box.style.transform = `translateX(${(i - currentBox) * 100}%)`;
+  });
+};
+
+moveBoxes(currentBox);
+
+nextBtn.addEventListener("click", function () {
+  if (currentBox >= questionBoxes.length - 1) {
+    return;
+  }
+  currentBox++;
+  moveBoxes(currentBox);
+});
+
+prevBtn.addEventListener("click", function () {
+  // console.log(currentBox);
+  if (currentBox <= 0) {
+    return;
+  }
+  currentBox--;
+  moveBoxes(currentBox);
+});
